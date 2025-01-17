15f1ab83082be0f644fa284317e757b569de67c0
Anikesh20 <anikeshsingh420@gmail.com>
2025-01-17 22:22:39 +0545

    Refactor routing in App.js to clean up SignIn component import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Components/Homepage";
 import Login from "./Components/Login";
 import ForgotPassword from "./Components/ForgotPassword";
-import SignIn from "./Components/SignIn"; // Assuming this is your SignUp page component
+import SignIn from "./Components/SignIn"; 
 import Contact from "./Components/Contact";
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/signup" element={<SignIn />} /> {/* Add SignUp route */}
+        <Route path="/signup" element={<SignIn />} /> 
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
